f44e1b23daa308f6d99f0e8a528ec8d9647703ca
webdevsmart <webdevsmart@hotmail.com>
2022-08-20 23:02:34 +0900

    fix: scripts

--- auto3.js
+++ auto3.js
@@ -1,28 +1,40 @@
 var config = {
-  baseBet: { label: "base bet", value: 10, type: "number" },
+  baseBet: { label: "base bet", value: 1, type: "number" },
   payout: { label: "payout", value: 2, type: "number" },
   invalidBetAmount: { label: "bet Amount of continouse 0", value: 0.0001, type: "number" },
 };
+
 function main() {
-  var currentBet = config.baseBet.value;
-  var loseCount = 0;
-  var winCount = 0;
+  var currentBet = config.invalidBetAmount.value;
+  var lastx = ["1", "1", "1", "1", "1"];
+  var xxx = 1;
+  var max_xxx = 1;
+
   game.onBet = function () {
     game.bet(currentBet, config.payout.value).then(function (payout) {
-      if (payout > 1) {
-        winCount++;
-        if (loseCount == 1 && winCount == 1) {
-          currentBet = config.baseBet.value;
+      lastx.shift();
+      lastx.push(payout > 1 ? '1' : '0')
+      log.success('lastx: ' + lastx.join(''))
+
+      if (currentBet == config.baseBet.value * xxx) {
+        if (payout > 1) {
+          log.success('win xxx: ' + xxx)
+          xxx = 1;
         } else {
-          currentBet = config.invalidBetAmount.value;
+          log.error('lose xxx:' + xxx)
+          xxx *= 2;
+          if (xxx > max_xxx)
+            max_xxx = xxx
         }
-        loseCount = 0;
-        winCount = 0;
+  
+        lastx = ["1", "1", "1", "1", "1"];
+      }
+
+      if (lastx.slice(1, 5).join('') === "1000") {
+        currentBet = config.baseBet.value * xxx;
       } else {
-        loseCount = 1;
         currentBet = config.invalidBetAmount.value;
       }
-      log.success("next bet: " + currentBet)
     });
   };
 }
