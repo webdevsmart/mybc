dfced07cd324708494b385f934c16e28492f733f
webdevsmart <webdevsmart@hotmail.com>
2022-10-21 01:35:03 +0900

    fix: get hash

---
+++ hash.js
@@ -0,0 +1,31 @@
+var CryptoJS = require("crypto-js");
+const fs = require('fs');
+
+let cycle = 1000000;
+let hashes = Array(cycle);
+let i = 0;
+let hash = "1992092119670422196611551989071919941021199801101994091719940225";
+let target = "ec08edd6094129465154ed7384020658c21b941e63ac9ce014689de25942a301";
+
+calc = () => {
+  while(true) {
+    hashes[i] = hash;
+    i++;
+    hash = String(CryptoJS.SHA256(String(hash)));
+    if (i == cycle) {
+      fs.writeFileSync("./hash.txt", hashes.join(","));
+      i = 0;
+    }
+    if (hash == target) {
+      hashes[i] = hash;
+      for (i = i + 1; i < cycle; i++) {
+        hashes[i] = "";
+      }
+      fs.writeFileSync("./hash1.txt", hashes.join(","));
+      console.log("found hash successfully")
+      break;
+    }
+  }
+}
+
+calc();
